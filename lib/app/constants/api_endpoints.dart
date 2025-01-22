815b8cd6a9fa3f6c0a9cc7663eb54d4d215b6996
Subekshya <subekshyakayastha4@gmail.com>
2025-01-22 09:44:21 +0545

    api endpoint and api service

---
+++ lib/app/constants/api_endpoints.dart
@@ -0,0 +1,17 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration receiveTimeout = Duration(seconds: 1000);
+  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  // static const String baseUrl = "http://localhost:3000/api/v1/";
+
+  static const String login = "auth/login";
+
+  static const String createBatch = "batch/createBatch";
+  static const String getAllBatch = "batch/getAllBatches";
+
+  static const String createCourse = "course/createCourse";
+  static const String deleteCourse = "course/";
+  static const String getAllCourse = "course/getAllCourse";
+}
